f68eb70870d333825d9dfbc5f9796934a4bc7463
Tanmay Jain <jaintanmay121@gmail.com>
2020-03-28 17:54:17 +0530

    Delete config.py

--- Project 4- Wrangle and Analyze Data/config.py
+++
@@ -1,6 +0,0 @@
-
-Access_Token = "1153265019154362368-c4iPvuioPTwA0BfT86diWl5Z1nHuBg"
-Access_Token_Secret =  "vM8dwNStiNzrbWe02rhOsETYBq95ZMmmssOqKHC1dShor"
-
-Consumer_Key = "ueut8Fw0SWXLvp7YsiHyvZih7"
-Consumer_Secret = "yL8nucRKz1kwXne0pDoLUlcKziJe06qj4acxdq4uAJj0M15eav"
